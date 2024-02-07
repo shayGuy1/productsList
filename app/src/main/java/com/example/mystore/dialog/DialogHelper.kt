3f90bff2e86b3eadd65d8800727d2a5d2008680f
AsherHomez <asher@homez.com>
2024-02-07 23:03:48 +0200

    Add more strings...

--- app/src/main/java/com/example/mystore/dialog/DialogHelper.kt
+++ app/src/main/java/com/example/mystore/dialog/DialogHelper.kt
@@ -2,6 +2,7 @@ package com.example.mystore.dialog
 
 import android.app.AlertDialog
 import android.content.Context
+import com.example.mystore.R
 
 object DialogHelper {
     enum class ResultCode {
@@ -11,11 +12,11 @@ object DialogHelper {
         AlertDialog.Builder(context)
             .setTitle(title)
             .setMessage(message)
-            .setPositiveButton("Retry") { dialog, _ ->
+            .setPositiveButton(context.getString(R.string.retry)) { dialog, _ ->
                 listener.invoke(ResultCode.Retry)
                 dialog.dismiss()
             }
-            .setNegativeButton("Cancel") { dialog, _ ->
+            .setNegativeButton(context.getString(R.string.cancel)) { dialog, _ ->
                 listener.invoke(ResultCode.Cancel)
                 dialog.dismiss()
             }
