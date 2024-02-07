651517ea5a0f86b8e166e5a7a5f98c2a0499fc00
AsherHomez <asher@homez.com>
2024-02-07 20:36:23 +0200

    Add network cache

--- app/src/main/java/com/example/mystore/api/maneger/RetrofitManager.kt
+++ app/src/main/java/com/example/mystore/api/maneger/RetrofitManager.kt
@@ -1,16 +1,23 @@
 package com.example.mystore.api.maneger
 
+import com.example.mystore.application.App
+import okhttp3.Cache
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitManager {
 
+    private const val CACHE_SIZE = 10 * 1024 * 1024L // 10MB
+
     private val retrofit : Retrofit = Retrofit.Builder()
         .baseUrl("https://dummyjson.com/")
         .addConverterFactory(GsonConverterFactory.create())
+        .client(OkHttpClient.Builder().cache(Cache(App.getContext().cacheDir, CACHE_SIZE)).build())
         .build()
 
     fun getInstance() : Retrofit {
        return retrofit
     }
 }
+
