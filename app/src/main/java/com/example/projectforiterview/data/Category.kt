2415eab05513db07e47baf5b978022043623aa68
AsherHomez <asher@homez.com>
2024-02-07 10:38:30 +0200

    Add data models

--- app/src/main/java/com/example/projectforiterview/data/Category.kt
+++ app/src/main/java/com/example/projectforiterview/data/Category.kt
@@ -1,3 +1,8 @@
 package com.example.projectforiterview.data
 
-data class Category(val name : String, val imageUrl : String, val totalDistinct : Int, val totalStock : Int)
+data class Category(
+    val name : String,
+    val imageUrl : String,
+    val totalDistinct : Int,
+    val totalStock : Int,
+    val products: List<Product>)
---
+++ app/src/main/java/com/example/projectforiterview/data/Product.kt
@@ -0,0 +1,7 @@
+package com.example.projectforiterview.data
+
+data class Product(
+    val name : String,
+    val imageUrl : String,
+    val price : Double,
+    val stockQuantity : Int)
